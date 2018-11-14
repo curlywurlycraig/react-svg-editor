d01d13c551146b8099ff4e33ed4cfaa1f4b9b6a4
Craig Wilkinson <craig@dangerfarms.com>
2018-11-14 21:24:31 +0000

    Added rudimentary code editor and svg display-er

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import brace from 'brace';
+import AceEditor from 'react-ace';
+
+import 'brace/mode/svg';
+import 'brace/theme/solarized_dark';
+
+import styles from './App.module.css';
 
 class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      svgCode: "<svg></svg>",
+      aceWidth: 1000,
+      aceHeight: 600,
+    }
+
+    this.aceRef = React.createRef();
+  }
+
+  componentDidMount() {
+    window.addEventListener('resize', this.updateWindowDimensions);
+  }
+
+  onChangeSvgText = newText => {
+    this.setState({
+      svgCode: newText,
+    });
+  };
+
+  updateWindowDimensions = () => {
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className={styles.container}>
+        <AceEditor
+          mode="svg"
+          theme="solarized_dark"
+          onChange={this.onChangeSvgText}
+          width={`${this.state.aceWidth}px`}
+          height={`${this.state.aceHeight}px`}
+          value={this.state.svgCode}
+          ref={this.aceRef}
+        />
+
+        <div
+          id="svgContainer"
+          dangerouslySetInnerHTML={{ __html: this.state.svgCode }}
+        />
       </div>
     );
   }
