d298876b8b342711cb60889ffe9cc37fa2dc5ea8
Craig Wilkinson <craig@dangerfarms.com>
2019-04-29 23:11:36 +0100

    Display control points on lines too

--- src/utils/svgGuide.js
+++ src/utils/svgGuide.js
@@ -18,7 +18,11 @@ export function generateGuideSvgSegment(command) {
         case 'V':
         case 'H':
         case 'L':
-            return <line x1={command.x0} y1={command.y0} x2={command.x} y2={command.y} stroke="black" />;
+        return <React.Fragment>
+            <line x1={command.x0} y1={command.y0} x2={command.x} y2={command.y} stroke="black" />
+            <circle className={styles.terminal_point} cx={command.x} cy={command.y} strokeWidth="1" stroke={terminalStroke} fill={terminalFill} r="8px" />
+            <circle className={styles.terminal_point} cx={command.x0} cy={command.y0} strokeWidth="1" stroke={terminalStroke} fill={terminalFill} r="8px" />
+        </React.Fragment>;
         case 'C':
             return <React.Fragment>
                 <line x1={command.x0} y1={command.y0} x2={command.x1} y2={command.y1} stroke="grey" />
@@ -27,15 +31,15 @@ export function generateGuideSvgSegment(command) {
                 <path d={`M${command.x0}, ${command.y0} C${command.x1}, ${command.y1}, ${command.x2}, ${command.y2}, ${command.x}, ${command.y}`} stroke="black" fill="none" />
                 <circle className={styles.terminal_point} cx={command.x0} cy={command.y0} strokeWidth="1" stroke={terminalStroke} fill={terminalFill} r="8px" />
                 <circle className={styles.terminal_point} cx={command.x} cy={command.y} strokeWidth="1" stroke={terminalStroke} fill={terminalFill} r="8px" />
-                <circle className={styles.terminal_point} cx={command.x1} cy={command.y1} strokeWidth="1" stroke={controlStroke} fill={controlFill} r="8px" />
-                <circle className={styles.terminal_point} cx={command.x2} cy={command.y2} strokeWidth="1" stroke={controlStroke} fill={controlFill} r="8px" />
+                <circle className={styles.control_point} cx={command.x1} cy={command.y1} strokeWidth="1" stroke={controlStroke} fill={controlFill} r="8px" />
+                <circle className={styles.control_point} cx={command.x2} cy={command.y2} strokeWidth="1" stroke={controlStroke} fill={controlFill} r="8px" />
             </React.Fragment>;
         case 'S':
             return <React.Fragment>
                 <line x1={command.x0} y1={command.y0} x2={command.x2} y2={command.y2} stroke="grey" />
                 <line x1={command.x2} y1={command.y2} x2={command.x} y2={command.y} stroke="grey" />
                 <path d={`M${command.x0}, ${command.y0} S${command.x2}, ${command.y2}, ${command.x}, ${command.y}`} stroke="black" fill="none" />
-                <circle className={styles.terminal_point} cx={command.x2} cy={command.y2} strokeWidth="1" stroke={controlStroke} fill={controlFill} r="8px" />
+                <circle className={styles.control_point} cx={command.x2} cy={command.y2} strokeWidth="1" stroke={controlStroke} fill={controlFill} r="8px" />
                 <circle className={styles.terminal_point} cx={command.x0} cy={command.y0} strokeWidth="1" stroke={terminalStroke} fill={terminalFill} r="8px" />
                 <circle className={styles.terminal_point} cx={command.x} cy={command.y} strokeWidth="1" stroke={terminalStroke} fill={terminalFill} r="8px" />
             </React.Fragment>;
