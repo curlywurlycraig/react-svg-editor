2e84a8dc081fe8145e7c6e96cdb65f06cba609f5
Craig Wilkinson <craig@dangerfarms.com>
2019-04-29 22:26:58 +0100

    Make the points look nicer

--- src/utils/svgGuide.js
+++ src/utils/svgGuide.js
@@ -1,5 +1,10 @@
 import React from "react";
 
+const terminalFill = "#d8d8d8";
+const terminalStroke = "#999999";
+const controlFill = "#e09191";
+const controlStroke = "#a36b6b";
+
 /**
  Assumes that the input command is always absolute.
  I only ever call this with commands generated with the makeAbsolute command used.
@@ -19,15 +24,19 @@ export function generateGuideSvgSegment(command) {
                 <line x1={command.x1} y1={command.y1} x2={command.x2} y2={command.y2} stroke="grey" />
                 <line x1={command.x2} y1={command.y2} x2={command.x} y2={command.y} stroke="grey" />
                 <path d={`M${command.x0}, ${command.y0} C${command.x1}, ${command.y1}, ${command.x2}, ${command.y2}, ${command.x}, ${command.y}`} stroke="black" fill="none" />
-                <circle cx={command.x1} cy={command.y1} strokeWidth="0" fill="red" r="4px" />
-                <circle cx={command.x2} cy={command.y2} strokeWidth="0" fill="red" r="4px" />
+                <circle cx={command.x0} cy={command.y0} strokeWidth="1" opacity="0.7" stroke={terminalStroke} fill={terminalFill} r="8px" />
+                <circle cx={command.x} cy={command.y} strokeWidth="1" opacity="0.7" stroke={terminalStroke} fill={terminalFill} r="8px" />
+                <circle cx={command.x1} cy={command.y1} strokeWidth="1" opacity="0.7" stroke={controlStroke} fill={controlFill} r="8px" />
+                <circle cx={command.x2} cy={command.y2} strokeWidth="1" opacity="0.7" stroke={controlStroke} fill={controlFill} r="8px" />
             </React.Fragment>;
         case 'S':
             return <React.Fragment>
                 <line x1={command.x0} y1={command.y0} x2={command.x2} y2={command.y2} stroke="grey" />
                 <line x1={command.x2} y1={command.y2} x2={command.x} y2={command.y} stroke="grey" />
                 <path d={`M${command.x0}, ${command.y0} S${command.x2}, ${command.y2}, ${command.x}, ${command.y}`} stroke="black" fill="none" />
-                <circle cx={command.x2} cy={command.y2} strokeWidth="0" fill="red" r="4px" />
+                <circle cx={command.x2} cy={command.y2} strokeWidth="1" opacity="0.7" stroke={controlStroke} fill={controlFill} r="8px" />
+                <circle cx={command.x0} cy={command.y0} strokeWidth="1" opacity="0.7" stroke={terminalStroke} fill={terminalFill} r="8px" />
+                <circle cx={command.x} cy={command.y} strokeWidth="1" opacity="0.7" stroke={terminalStroke} fill={terminalFill} r="8px" />
             </React.Fragment>;
         default:
             return null;
